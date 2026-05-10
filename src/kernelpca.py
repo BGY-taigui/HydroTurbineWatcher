031f5efc5b4b6a07f293476230b49539bc051ddc
BGY <tyamanakatyamanaka@gmail.com>
2026-05-11 00:31:59 +0900

    implimenting new kernel PCA algorizm but not complited. normalizing alpha vec is needed

--- src/kernelpca.py
+++ src/kernelpca.py
@@ -8,12 +8,15 @@ from sklearn.decomposition import KernelPCA as SKKernelPCA
 class KernelPCA:
     def __init__(self, data, n_components,kernel="rbf", gamma=1):
 
+        self.gamma = gamma
 
         self.X = np.array(data)
         self.Xmean = np.mean(self.X, axis=0)
         self.Xstd = self.X.std(axis=0,ddof=1)
 
-        Xc = (self.X - self.Xmean) / self.Xstd
+        self.Xc = (self.X - self.Xmean) / self.Xstd
+
+        self.N = len(self.Xc)
 
         self.kpca = SKKernelPCA(
             n_components=n_components,
@@ -22,10 +25,46 @@ class KernelPCA:
             fit_inverse_transform=True
         )
 
-        Z = self.kpca.fit_transform(Xc)
+        Z = self.kpca.fit_transform(self.Xc)
 
         self.eigvals = self.kpca.eigenvalues_
         self.eigvecs = self.kpca.eigenvectors_
+
+
+        self.K_matrix = np.array([[self.centered_kernel_function(xi, xj) for xj in self.Xc] for xi in self.Xc])
+
+        self.eigen_values, self.eigen_vectors = np.linalg.eig(self.K_matrix)
+
+
+
+    def centered_kernel_function(self, x, y):
+
+        kxy = self.k_function(x, y)
+        kxxc = np.mean([self.k_function(x, xi) for xi in self.Xc])
+        kyxc = np.mean([self.k_function(xi, y) for xi in self.Xc])
+        kxcxc = np.mean([self.k_function(xi, xj) for xi in self.Xc for xj in self.Xc])
+
+        return kxy - kxxc - kyxc + kxcxc
+
+    def k_function(self,x,y):
+        return np.exp(-self.gamma * np.sum((np.array(x) - np.array(y)) ** 2))
+
+    def alpha_vectors(self):
+        #TODO Phiにした時に固有ベクトルが正規化されるようにする。
+        return self.eigvecs
+
+    def lambda_values(self):
+        #return self.eigvals / self.N
+        return self.eigvals
+        #TODO ラムダの値はあってた
+
+    def score(self,data,eigenvec_index):
+        data_c = (data - self.Xmean) / self.Xstd
+
+        alpha_vec = self.alpha_vectors()[:, eigenvec_index]
+        kernel_vec = np.array([self.centered_kernel_function(data_c, xi) for xi in self.Xc   ])
+
+        return np.dot(alpha_vec, kernel_vec)
 
     def show_cum_ratio(self):
         plt.plot(self.eigvals/sum(self.eigvals),label="eigenvalues")
@@ -55,9 +94,27 @@ class KernelPCA:
         data_c = (data - self.Xmean) / self.Xstd
         Z = self.kpca.transform(data_c.reshape(1, -1))[:,:components_num]
         mu = self.eigvals[:components_num]
+        
+        print("Z",Z)
+        print("mu",mu)
 
         T2 = np.sum((Z**2) / mu)
         return T2
+
+
+    def get_T2_2(self,data,components_num):
+        scores = [self.score(data,index)**2/self.lambda_values()[index] for index in range(components_num)]
+
+        print("scores",[self.score(data,index) for index in range(components_num)])
+        print("lambda_values",[self.lambda_values()[index] for index in range(components_num)])
+
+
+        return sum(scores)
+
+
+    def get_Q_2(self,data,components_num):
+
+        pass
 
     def get_each_T2(self,data,eignvec_index):
         data_c = (data - self.Xmean) / self.Xstd
@@ -69,37 +126,21 @@ class KernelPCA:
 
     def get_Q(self,data,components_num):
         #TODO 未実装
-        data_mean = data - self.Xmean
-        data_std = data_mean / self.Xstd
+        data_c = (data - self.Xmean) / self.Xstd
+        print("data_c",data_c)
+        print("data_c",data_c.reshape(1, -1))
+        print(self.kpca._get_kernel(data_c.reshape(1, -1)))
+        print(self.kpca._get_kernel(self.Xc))
 
-        data_reconstructed = self.eigvecs[:,:components_num] @ (self.eigvecs[:,:components_num].T @ data_std.T)
-        residual = data_std - data_reconstructed
+        Q = 0
 
-        Q = np.sum(residual**2)
+        #data_reconstructed = self.eigvecs[:,:components_num] @ (self.eigvecs[:,:components_num].T @ data_std.T)
+        #residual = data_std - data_reconstructed
+
+        #Q = np.sum(residual**2)
 
         return Q
 
-    def show_T2_report(self,data,components_num):
-        #TODO 未実装 
-        data_std = (data-self.Xmean)/self.Xstd
-        score = np.dot(data_std , self.eigvecs[:,:components_num])
-
-        T2_vec =  (score**2) / self.eigvals[:components_num]
-
-        plt.plot(T2_vec)
-        plt.show()
-
-
-    def show_Q_report(self,data,components_num):
-        #TODO 未実装
-        data_mean = data - self.Xmean
-        data_std = data_mean / self.Xstd
-
-        data_reconstructed = self.eigvecs[:,:components_num] @ (self.eigvecs[:,:components_num].T @ data_std.T)
-        residual = data_std - data_reconstructed
-
-        plt.plot(residual)
-        plt.show()
 
     def get_T2_limit(self,components_num):
 
